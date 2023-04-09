0885ca81ea20e813a931b27531a1c62ae39c8c00
Rafał Peisert <rafalmp@users.noreply.github.com>
2023-04-09 19:34:59 +0200

    Custom paths

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/rafalmp/cyoa"
 )
@@ -28,7 +30,40 @@ func main() {
 		os.Exit(1)
 	}
 
-	h := cyoa.NewHandler(story)
+	tpl := template.Must(template.New("").Parse(customTemplate))
+
+	h := cyoa.NewHandler(story, cyoa.WithTemplate(tpl), cyoa.WithPathFunc(pathFn))
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
 	fmt.Printf("Starting the server on port %d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
 }
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+	return path[len("/story/"):]
+}
+
+var customTemplate = `
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choose Your Own Adventure</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+        {{range .Options}}
+        <li><a href="/story/{{.Arc}}">{{.Text}}</a></li>
+        {{end}}
+    </ul>
+</body>
+</html>`
